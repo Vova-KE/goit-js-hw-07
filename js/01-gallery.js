57c5a375663ea4645b6e395f6a95831a4af65d27
Volodymyr Kozyr <vroger4@gmail.com>
2022-05-11 20:18:39 +0300

    homework#7
    task 1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const createGalleryMarkup = ({original, preview, description}) => {
+    return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+    </div>`;
+};
+
+const galleryMarkup = galleryItems.map(createGalleryMarkup).join('');
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+
+const handleClickImage = (event) => {
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    event.preventDefault();
+
+    const onEscKeyPress = (event) => {
+    if (event.code === 'Escape') {
+        originImage.close();
+        }   
+    }
+
+    const originImage = basicLightbox
+        .create(`<img src="${event.target.dataset.source}">`,
+        {closable: true,
+    onShow: (originImage) => {
+        window.addEventListener('keydown', onEscKeyPress)
+    },
+    onClose: (originImage) => {
+        window.removeEventListener('keydown', onEscKeyPress)
+    },
+        });
+    
+    originImage.show();
+}
+
+gallery.addEventListener('click', handleClickImage);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+// console.log(gallery);
+
+const createGalleryMarkup = ({original, preview, description}) => {
+    return `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`
+};
+
+const galleryMarkup = galleryItems.map(createGalleryMarkup).join('');
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+const handleClickImage = (event) => {
+    event.preventDefault();
+
+}
+
+gallery.addEventListener('click', handleClickImage);
+
